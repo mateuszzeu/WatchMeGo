2519c995ebe832a718f3b652c42ce4cd960ecfae
Mat <mateuszzeu@gmail.com>
2025-05-22 18:36:46 +0200

    Initial Commit

---
+++ WatchMeGo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WatchMeGo
+//
+//  Created by Liza on 22/05/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
