a86f39ceecd941f3facc7df8bd7007beeb421bab
Mat <mateuszzeu@gmail.com>
2025-07-18 18:05:20 +0200

    Initial Commit

---
+++ WatchMeGo/WatchMeGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchMeGoApp.swift
+//  WatchMeGo
+//
+//  Created by Liza on 18/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct WatchMeGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WatchMeGoUITests/WatchMeGoUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  WatchMeGoUITestsLaunchTests.swift
+//  WatchMeGoUITests
+//
+//  Created by Liza on 18/07/2025.
+//
+
+import XCTest
+
+final class WatchMeGoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
